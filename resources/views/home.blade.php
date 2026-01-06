6133b3cdab4dfe61eedbe725e177941e572271ee
LennoNardo <nardolenno@gmail.com>
2026-01-06 18:05:11 +0700

    fix: Update button class for improved styling on product catalog link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -148,7 +148,7 @@
                 <div
                     class="absolute bottom-3 right-3 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity">
                     <a href="{{ route('catalog.show', $product) }}"
-                        class="bg-white text-primary p-2.5 rounded-full shadow-lg hover:bg-primary hover:text-white transition">
+                        class=" text-primary p-2.5 rounded-full shadow-lg hover:text-white transition">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
